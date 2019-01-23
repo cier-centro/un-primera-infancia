0b5b205b908e2a706a650c3a71e808121883b010
DESKTOP-PQ4K3JU\Contenidos <yiye_91@hotmail.com>
2019-01-23 17:16:46 -0500

    Cambios en estilos de header y menu principal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
+  $('nav a.inside-link').click(function(e) {
+    e.preventDefault();
+    var strAnchor = $(this).attr('href');
+    $('body,html').stop(true, true).animate({
+      scrollTop: $(strAnchor).offset().top
+    }, 900);
+  });
 });
