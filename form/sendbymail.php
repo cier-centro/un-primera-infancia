f10ffc268598d4dc80280db73196d9fe537709d5
DESKTOP-PQ4K3JU\Contenidos <yiye_91@hotmail.com>
2019-02-07 12:10:52 -0500

    Se avanza estructura en banner y se arregla funcionalidad de botones y fomulario

--- form/sendbymail.php
+++ form/sendbymail.php
@@ -4,7 +4,7 @@ include_once('PHPMailer/class.phpmailer.php');
 include_once('PHPMailer/class.smtp.php');
 
 if (isset($_POST['email'])) {
-    
+
     if (!isset($_POST['name']) || !isset($_POST['phone']) || !isset($_POST['email'])) {
         echo'<script type="text/javascript">
                 alert("Favor diligencie todos los datos.");
@@ -12,7 +12,7 @@ if (isset($_POST['email'])) {
              </script>';
         return false;
     }
-    
+
     $mail = new PHPMailer();
     $mail->IsSMTP();
     $mail->SMTPAuth = true;
@@ -23,31 +23,29 @@ if (isset($_POST['email'])) {
         En configuracion del gmail remitente->Seguridad->Permitir aplicaciones no seguras->activar
     */
     $mail->Username ='remitente@gmail.com';
-    $mail->Password = 'claveremitente'; 
-    
+    $mail->Password = 'claveremitente';
+
     $message = "";
     $to = "destinatario@gmail.com";
     $subject = 'Formulario de contacto';
-    
+
     $message = "Detalles del formulario de contacto: <br>";
     $message .= "Nombre: " . $_POST['name'] . "<br>";
     $message .= "Teléfono: " . $_POST['phone'] . "<br>";
     $message .= "E-mail: " . $_POST['email'] . "<br>";
     $message .= "Sugerencias: " . $_POST['messageSuggestion'] . "<br>";
-    
+
     $mail->AddAddress($to);
     $mail->Subject = $subject;
     $mail->Body = $message;
     $mail->MsgHTML($message);
-    
+
     if($mail->Send()){
         echo '<script type="text/javascript">
-                console.log("Enviado Correctamente");
                 window.location = "index.html"
              </script>';
     }else{
-        echo '<script type="text/javascript">
-                console.log("NO ENVIADO, intentar de nuevo");
+        echo '<script type="text/javascript">                
              </script>';
     }
 }
